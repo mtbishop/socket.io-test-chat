df9ff0d5922698232fce21dbac90d6d7a7172fb6
mtbishop <bishopmatthew76@gmail.com>
2021-03-22 20:58:05 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App setup
+
+var app = express();
+var server = app.listen(4000, function () {
+  console.log('listening to requests on http://localhost:4000');
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup
+var io = socket(server);
+
+io.on('connection', function (socket) {
+  console.log('a socket connection has been made with', socket.id);
+
+  socket.on('chat', function (data) {
+    io.sockets.emit('chat', data);
+  });
+});
